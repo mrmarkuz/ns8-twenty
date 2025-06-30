cac9761f468dcf4657db6d663bf4a0c809d7f14a
mrmarkuz <mrmarkuz666@gmail.com>
2025-06-30 13:09:30 +0200

    Repository initialization

--- build-images.sh
+++ build-images.sh
@@ -13,22 +13,22 @@ images=()
 # The image will be pushed to GitHub container registry
 repobase="${REPOBASE:-ghcr.io/nethserver}"
 # Configure the image name
-reponame="kickstart"
+reponame="twenty"
 
 # Create a new empty container image
 container=$(buildah from scratch)
 
-# Reuse existing nodebuilder-kickstart container, to speed up builds
-if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-kickstart; then
+# Reuse existing nodebuilder-twenty container, to speed up builds
+if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-twenty; then
     echo "Pulling NodeJS runtime..."
-    buildah from --name nodebuilder-kickstart -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
+    buildah from --name nodebuilder-twenty -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
 fi
 
 echo "Build static UI files with node..."
 buildah run \
     --workingdir=/usr/src/ui \
     --env="NODE_OPTIONS=--openssl-legacy-provider" \
-    nodebuilder-kickstart \
+    nodebuilder-twenty \
     sh -c "yarn install && yarn build"
 
 # Add imageroot directory to the container image
